0478967bd7873e8e8160802cfa1e502ad58823c9
axkiss <axkiss@yandex.ru>
2022-03-12 16:55:57 +0700

    Refuse from full including urls of standard auth

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth.views import LogoutView
+
 
 app_name = 'users'
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
     path('login/', views.MyLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('password_reset/', views.MyPasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', views.MyPasswordResetDoneView.as_view(), name='password_reset_done'),
