9342908c69f1bad6879aa6636b9a241659d2e6a7
axkiss <axkiss@yandex.ru>
2022-03-14 13:51:46 +0700

    Add drop menu for user was logged in

--- users/models.py
+++ users/models.py
@@ -5,4 +5,8 @@ from django.db import models
 # Create your models here.
 
 class User(AbstractUser):
-    pass
+    def get_is_staff(self):
+        return self.is_staff
+
+    def get_perm_add_post(self):
+        return self.has_perm('blog_app.add_post')
