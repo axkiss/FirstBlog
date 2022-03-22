8a7bb586a454b1e5cab9152f9c4d8a4b1aa21f7a
axkiss <axkiss@yandex.ru>
2022-03-22 19:46:06 +0700

    Add popular post to sidebar

--- blog_app/templatetags/blog_app_tags.py
+++ blog_app/templatetags/blog_app_tags.py
@@ -1,13 +1,22 @@
 from django import template
 from blog_app.models import Post
+from django.utils import timezone
 
 register = template.Library()
 
 
 @register.simple_tag(name='list_tags')
-def get_main_tags(pos, cnt_head_tag, cnt_side_tag):
+def get_list_tags(pos, cnt_head_tag, cnt_side_tag):
     list_tags = Post.tag.most_common()
     if pos == 'head':
         return list_tags[:cnt_head_tag]
     else:
         return list_tags[cnt_head_tag:cnt_head_tag + cnt_side_tag]
+
+
+@register.simple_tag(name='popular_posts')
+def get_popular_posts(days, cnt_posts):
+    end_date = timezone.now()
+    start_date = end_date - timezone.timedelta(days=days)
+    return Post.objects.filter(
+        created_at__range=(start_date, end_date)).order_by('-views')[:cnt_posts]
