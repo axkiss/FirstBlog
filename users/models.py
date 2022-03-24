d5156a1d95187ba58023df897ecfba458a593bd1
axkiss <axkiss@yandex.ru>
2022-03-24 20:53:49 +0700

    Del excess code

--- users/models.py
+++ users/models.py
@@ -5,8 +5,6 @@ from django.db import models
 # Create your models here.
 
 class User(AbstractUser):
-    def get_is_staff(self):
-        return self.is_staff
 
-    def get_perm_add_post(self):
+    def has_perm_add_post(self):
         return self.has_perm('blog_app.add_post')
