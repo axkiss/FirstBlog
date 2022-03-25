75619d49c6ecde072fc8cbd7b33214cd9722e9ca
axkiss <axkiss@yandex.ru>
2022-03-25 17:19:38 +0700

    Add get group of user

--- users/models.py
+++ users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
 
 
@@ -8,3 +8,6 @@ class User(AbstractUser):
 
     def has_perm_add_post(self):
         return self.has_perm('blog_app.add_post')
+
+    def get_group(self):
+        return self.groups.all().first()
