440a20a71a2d4ce7c1cfc9e6b6a5cfb772b8ef04
axkiss <axkiss@yandex.ru>
2022-03-29 19:15:21 +0700

    Fix style

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.MainView.as_view(), name='index'),
     path('about/', views.AboutUsView.as_view(), name='about'),
-    path('add_post/', views.AddPostView.as_view(), name='add-post'),
+    path('add_post/', views.AddPostView.as_view(), name='add_post'),
     path('search/', views.SearchView.as_view(), name='search'),
-    path('<slug:slug>', views.PostDetailView.as_view(), name='post-detail'),
+    path('<slug:slug>', views.PostDetailView.as_view(), name='post_detail'),
     path('tag/<slug:slug>', views.TagView.as_view(), name='tag')
 ]
