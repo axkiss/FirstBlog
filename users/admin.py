0b25620ea7fcc1288cc7bae1c33c0196587fe21b
axkiss <axkiss@yandex.ru>
2022-03-29 18:28:15 +0700

    Add extra user profiles to admin

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+from .models import ExtraUserProfile
 
 User = get_user_model()
 
@@ -8,3 +9,8 @@ User = get_user_model()
 @admin.register(User)
 class UserAdmin(UserAdmin):
     pass
+
+
+@admin.register(ExtraUserProfile)
+class ExtraUserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'about_me')
