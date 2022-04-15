89e38983cc83c5c1ef140404d2d8153a0a4986f6
axkiss <axkiss@yandex.ru>
2022-04-15 21:39:34 +0700

    Add function delete avatar after replacing to a new

---
+++ users/signals.py
@@ -0,0 +1,24 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from users.models import ExtraUserProfile
+import os
+
+
+@receiver(pre_save, sender=ExtraUserProfile)
+def delete_old_avatar(sender, instance, **kwargs):
+    # not on the first creation
+    if not instance.id:
+        return False
+
+    try:
+        old_avatar = sender.objects.get(id=instance.id).avatar
+    except sender.DoNotExist:
+        return False
+
+    # compare avatars and delete the oldest
+    new_avatar = instance.avatar
+    if old_avatar and old_avatar != new_avatar:
+        if os.path.isfile(old_avatar.path):
+            os.remove(old_avatar.path)
+
+
