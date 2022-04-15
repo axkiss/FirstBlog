2bbf5edba6a0d80a711fe069b021f5c1ce2ebe91
axkiss <axkiss@yandex.ru>
2022-04-15 21:41:21 +0700

    Add function сrop to square and resize img

--- users/utils.py
+++ users/utils.py
@@ -4,6 +4,7 @@ from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import default_token_generator as token_generator
+from PIL import Image
 
 
 def send_email_for_verify(request, user):
@@ -32,3 +33,28 @@ def send_email_for_verify(request, user):
                          to=[user.email]
                          )
     email.send()
+
+
+def make_square_img(height_side, img_path):
+    """
+    Crop to square and resize
+    :param height_side:
+    :param img_path:
+    :return:
+    """
+    pic = Image.open(img_path)
+    if pic.width != pic.height:
+        # crop to center square
+        min_side = min(pic.width, pic.height)
+        if min_side == pic.height:
+            start_point = int((pic.width - pic.height) // 2)
+            area = (start_point, 0, start_point + pic.height, pic.height)
+            pic = pic.crop(area)
+        else:
+            start_point = int((pic.height - pic.width) // 2)
+            area = (0, start_point, pic.width, start_point + pic.width)
+            pic = pic.crop(area)
+
+    # resize
+    pic.thumbnail((height_side, height_side), Image.LANCZOS)
+    pic.save(img_path)
