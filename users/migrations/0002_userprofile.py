f1cab71d98b2c2eab9ef8480fa9b6aa93b80d9cc
axkiss <axkiss@yandex.ru>
2022-04-25 22:39:15 +0700

    Add: image size validator for avatar

--- users/migrations/0002_userprofile.py
+++ users/migrations/0002_userprofile.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
 import users.models
+import users.validators
 
 
 class Migration(migrations.Migration):
@@ -17,7 +18,7 @@ class Migration(migrations.Migration):
             name='ExtraUserProfile',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('avatar', models.ImageField(blank=True, default=None, null=True, upload_to=users.models.user_directory_path)),
+                ('avatar', models.ImageField(blank=True, default=None, null=True, upload_to=users.models.user_directory_path, validators=[users.validators.ImageSizeValidator(max_size=(1500, 1500), min_size=(65, 65))])),
                 ('about_me', models.TextField(blank=True, default=None, max_length=200, null=True)),
                 ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
