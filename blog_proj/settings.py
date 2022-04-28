fef4c54d9f0807f801ebfc50ec3d1a5671f34e48
axkiss <axkiss@yandex.ru>
2022-04-28 17:23:12 +0700

    Add: email backend settings

--- blog_proj/settings.py
+++ blog_proj/settings.py
@@ -37,8 +37,16 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST_USER = os.getenv('MAIN_EMAIL')
+    EMAIL_HOST_PASSWORD = os.getenv('GMAIL_HOST_PASSWORD')
 # Application definition
-EMAIL_FEEDBACK = 'axkiss@ya.ru'
+EMAIL_FEEDBACK = os.getenv('MAIN_EMAIL')
+MAIN_EMAIL = os.getenv('MAIN_EMAIL')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
