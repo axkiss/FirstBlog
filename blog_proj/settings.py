4b3b210044f7b8979febbbd931e44302d2fe6e5f
axkiss <axkiss@yandex.ru>
2022-05-01 11:16:34 +0700

    Migrate db backend from SQLite to PostgreSQL

--- blog_proj/settings.py
+++ blog_proj/settings.py
@@ -99,8 +99,12 @@ WSGI_APPLICATION = 'blog_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'firstblog',
+        'USER': 'firstuser',
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
