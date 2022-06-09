107808a5b73ce168f04490bceaa6bbf16ef96f2c
axkiss <axkiss@yandex.ru>
2022-06-09 18:24:59 +0700

    Move business logic to services.py

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,25 +1,18 @@
-import os
-import re
-from io import BytesIO
-from PIL import Image
-from django.core.exceptions import ObjectDoesNotExist, ValidationError
-from django.core.files.base import ContentFile
+from django.core.exceptions import ValidationError
 from django.core.validators import FileExtensionValidator
 from django.db import models
-from django.db.models import Q, F
-from django.utils.html import strip_tags
-
+from django.db.models import F
+from blog_app.services import create_thumbnail_for_post, get_unique_slug
 from users.models import User
 from django.urls import reverse
 from django.utils import timezone
 from ckeditor_uploader.fields import RichTextUploadingField
 from taggit.managers import TaggableManager
-
-from users.utils import crop_img_to_square
 from users.validators import ImageSizeValidator
 
 
 class SeoData(models.Model):
+    """Seo data of blog"""
     site_name = models.CharField(max_length=80)
     domain = models.CharField(max_length=80, help_text='For example: test.com')
     favicon = models.ImageField(validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png', 'ico']),
@@ -37,8 +30,9 @@ class SeoData(models.Model):
 
 
 class Post(models.Model):
+    """Posts on blog"""
     title = models.CharField(max_length=100)
-    url = models.SlugField(default='', null=False, db_index=True, max_length=80)
+    slug = models.SlugField(default='', null=False, db_index=True, max_length=80)
     description = RichTextUploadingField()
     image = models.ImageField(upload_to='post/%Y/%m/%d/',
                               validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png']),
@@ -50,108 +44,30 @@ class Post(models.Model):
     tag = TaggableManager()
     views = models.PositiveIntegerField(default=0)
 
+    class Meta:
+        ordering = ['-id']
+
     def __str__(self):
         return self.title
 
-    def get_url(self):
-        return reverse('blog:post_detail', args=[self.url])
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.slug])
 
     def add_one_view(self):
         Post.objects.filter(id=self.id).update(views=F('views') + 1)
         return None
 
-    @staticmethod
-    def get_results_search(search_query, len_desc_post=200, posts_on_page=10, max_pages_pagination=5):
-        # Find search query in database of posts
-        result_posts = Post.objects.filter(
-            Q(title__icontains=search_query) | Q(description__icontains=search_query)).order_by('-id') \
-            [:posts_on_page * max_pages_pagination]
-        if len(result_posts) == 0:
-            return ''
-        else:
-
-            # Highlight search query in results
-            for post in result_posts:
-
-                # Remove HTML tags
-                clean_desc = strip_tags(post.description)
-
-                # Truncate description of posts
-                begin = clean_desc.lower().find(search_query.lower())
-                end = begin + len(search_query)
-                if begin - len_desc_post // 2 > 0:
-                    if len(clean_desc) - end < len_desc_post // 2:
-                        new_begin = begin - (
-                                len_desc_post // 2 - (len(clean_desc) - end)) - len_desc_post // 2
-                        if new_begin < 0:
-                            short_desc = clean_desc
-                        else:
-                            short_desc = clean_desc[new_begin:]
-                    else:
-                        short_desc = clean_desc[begin - len_desc_post // 2:end + len_desc_post // 2]
-                else:
-                    short_desc = clean_desc[:end + len_desc_post - begin]
-                short_desc = '...' + short_desc.strip() + '...'
-
-                # Highlight all the entry
-                post.description = re.sub(f'({search_query})', r'<mark>\1</mark>',
-                                          short_desc,
-                                          flags=re.IGNORECASE)
-                post.title = re.sub(f'({search_query})', r'<mark>\1</mark>',
-                                    post.title, flags=re.IGNORECASE)
-            return result_posts
-
-    def create_thumbnail(self, height_side):
-        # If the image of the post did not change --> break
-        if self.thumbnail:
-            try:
-                old_image = Post.objects.get(id=self.id).image
-            except ObjectDoesNotExist:
-                return
-            if self.image.name == old_image.name:
-                return
-
-        # Get name for thumbnail
-        thumb_name, thumb_extension = os.path.splitext(self.image.name)
-        thumb_name = os.path.basename(thumb_name)
-        thumb_extension = thumb_extension.lower()
-        thumb_filename = thumb_name + '_thumb' + thumb_extension
-
-        # Define image file type
-        if thumb_extension in ['.jpg', '.jpeg']:
-            FTYPE = 'JPEG'
-        elif thumb_extension == '.png':
-            FTYPE = 'PNG'
-        else:
-            raise TypeError('Unrecognized image file type')
-
-        # Open, crop and resize image
-        pic = Image.open(self.image)
-        pic = crop_img_to_square(pic)
-        pic.thumbnail((height_side, height_side), Image.LANCZOS)
-
-        # Save thumbnail to in-memory file as StringIO
-        temp_thumb = BytesIO()
-        pic.save(temp_thumb, FTYPE)
-        temp_thumb.seek(0)
-
-        # set save=False, otherwise it will run in an infinite loop
-        self.thumbnail.save(thumb_filename, ContentFile(temp_thumb.read()), save=False)
-        temp_thumb.close()
-
     def save(self, *args, **kwargs):
-        # make unique url of post
-        if Post.objects.filter(Q(url=self.url) & ~Q(id=self.id)).exists():
-            msec = str(timezone.now().microsecond)
-            self.url = self.url[:73] + '-' + msec
-        self.url = self.url[:80]
+        # make unique slug of post
+        self.slug = get_unique_slug(self, max_length=80)
 
         # create thumbnail from post image
-        self.create_thumbnail(100)
+        create_thumbnail_for_post(self, height_side=100)
         super(Post, self).save(*args, **kwargs)
 
 
 class Comment(models.Model):
+    """Comments under the post"""
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_name')
     text = models.TextField(max_length=500)
