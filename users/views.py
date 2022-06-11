11fd5d2fd7b3d38468b7b21836df3157a2364364
axkiss <axkiss@yandex.ru>
2022-06-11 15:38:54 +0700

    Optimisation, use of Class-based views

--- users/views.py
+++ users/views.py
@@ -1,95 +1,65 @@
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.views import LoginView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, \
     PasswordResetCompleteView
-from django.contrib.auth.models import Group
-from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
-from django.utils.http import urlsafe_base64_decode
 from django.views import View
-from django.contrib.auth.tokens import default_token_generator as token_generator
+from django.views.generic import DetailView, TemplateView, FormView
 
 from blog_app.models import SeoData
 from .forms import MyAuthenticationForm, MyUserCreationForm, MyPasswordResetForm, MySetPasswordForm, EditUserForm, \
     EditExtraUserProfileForm
 from .models import User, ExtraUserProfile
+from .services import check_user_and_token, add_user_to_group, get_user_by_uidb
 from .utils import send_email_for_verify
 
 
 class MyLoginView(LoginView):
+    """Login and authenticate user"""
     form_class = MyAuthenticationForm
     template_name = 'users/login.html'
 
 
-class RegisterView(View):
+class RegisterView(FormView):
+    """Register, authenticate and send email for verify new user"""
     template_name = 'users/register.html'
+    form_class = MyUserCreationForm
+    success_url = reverse_lazy('users:email_confirm')
 
-    def get(self, request):
-        form = MyUserCreationForm()
-        context = {
-            'form': form
-
-        }
-        return render(request, template_name=self.template_name, context=context)
-
-    def post(self, request):
-        form = MyUserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            send_email_for_verify(request, user)
-            return redirect('users:email_confirm')
-        context = {
-            'form': form
-        }
-        return render(request, template_name=self.template_name, context=context)
+    def form_valid(self, form):
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        send_email_for_verify(self.request, user)
+        return super(RegisterView, self).form_valid(form)
 
 
-class EmailConfirmView(View):
+class EmailConfirmView(TemplateView):
+    """Show message about sending mail for verify"""
     template_name = 'users/email_confirm.html'
-
-    def get(self, request):
-        return render(request, template_name=self.template_name)
 
 
 class EmailConfirmVerifyView(View):
+    """Confirm user email or show message about an invalid link """
 
     def get(self, request, uidb64, token):
-        user = self.get_user(uidb64)
-        if user is not None \
-                and token_generator.check_token(user, token):
-            user.email_verify = True
+        user = get_user_by_uidb(uidb64)
+        if check_user_and_token(user, token):
             user.save()
             login(request, user)
-            # Add extra profile to user
-            ExtraUserProfile.objects.get_or_create(user=user)
-            # Add user to group 'Reader'
-            group, created = Group.objects.get_or_create(name='Reader')
-            user.groups.add(group)
+            add_user_to_group(user, group_name='Reader')
             return redirect('users:profile', username=user.username)
         return redirect('users:email_confirm_invalid')
 
-    @staticmethod
-    def get_user(uidb64):
-        try:
-            # urlsafe_base64_decode() decodes to bytestring
-            uid = urlsafe_base64_decode(uidb64).decode()
-            user = User.objects.get(pk=uid)
-        except (TypeError, ValueError, OverflowError, User.DoesNotExist, ValidationError):
-            user = None
-        return user
 
-
-class EmailConfirmInvalidView(View):
+class EmailConfirmInvalidView(TemplateView):
+    """Show message about invalid verify"""
     template_name = 'users/email_confirm_invalid.html'
-
-    def get(self, request):
-        return render(request, template_name=self.template_name)
 
 
 class MyPasswordResetView(PasswordResetView):
+    """Show form for password reset. Processing request"""
     email_template_name = 'users/password_reset_email.html'
     seo_data = SeoData.objects.first()
     extra_email_context = {'domain': seo_data.domain, 'site_name': seo_data.site_name} if seo_data else {}
@@ -99,31 +69,34 @@ class MyPasswordResetView(PasswordResetView):
 
 
 class MyPasswordResetDoneView(PasswordResetDoneView):
+    """Show a message that an instruction for password reset has been send"""
     template_name = 'users/password_reset_done.html'
 
 
 class MyPasswordResetConfirmView(PasswordResetConfirmView):
+    """Show form to set new password"""
     form_class = MySetPasswordForm
     template_name = 'users/password_reset_confirm.html'
     success_url = reverse_lazy('users:password_reset_complete')
 
 
 class MyPasswordResetCompleteView(PasswordResetCompleteView):
+    """Show message about success reset password"""
     template_name = 'users/password_reset_complete.html'
 
 
-class UserProfileView(View):
+class UserProfileDetailView(DetailView):
+    """Show user profile"""
     template_name = 'users/profile.html'
+    model = User
+    context_object_name = 'user_profile'
 
-    def get(self, request, username):
-        user = get_object_or_404(User, username=username)
-        context = {
-            'user_profile': user
-        }
-        return render(request, template_name=self.template_name, context=context)
+    def get_object(self, **kwargs):
+        return get_object_or_404(User, username=self.kwargs.get('username'))
 
 
 class EditUserProfileView(View):
+    """Show a page for updating information about the user profile"""
     template_name = 'users/edit_profile.html'
 
     def get(self, request, username):
