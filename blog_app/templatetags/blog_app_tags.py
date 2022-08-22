584a64b32156e89f7810246efdcac2de269cc673
axkiss <axkiss@yandex.ru>
2022-08-22 13:30:28 +0700

    Fix: optimization query in popular posts

--- blog_app/templatetags/blog_app_tags.py
+++ blog_app/templatetags/blog_app_tags.py
@@ -18,14 +18,18 @@ def get_list_tags(pos, cnt_head_tag, cnt_side_tag):
 @register.simple_tag(name='popular_posts')
 def get_popular_posts(days, cnt_posts):
     """Show popular post in n days"""
+
+    last_post = Post.objects.last()
     # if blog hasn't publications
-    if not Post.objects.last():
+    if last_post is None:
         return ''
-    end_date = Post.objects.last().created_at
+
+    end_date = last_post.created_at
     start_date = end_date - timezone.timedelta(days=days)
     popular_posts = Post.objects.filter(
-        created_at__range=(start_date, end_date)).order_by('-views')[:cnt_posts]
+        created_at__range=(start_date, end_date)).order_by('-views').only('title', 'slug', 'thumbnail')[:cnt_posts]
     # if no publications for a long time
     if len(popular_posts) < cnt_posts:
-        popular_posts = Post.objects.order_by('-views', '-created_at')[:cnt_posts]
+        popular_posts = Post.objects.order_by('-views', '-created_at').only('title', 'slug', 'thumbnail')[:cnt_posts]
+
     return popular_posts
